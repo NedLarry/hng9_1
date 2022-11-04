9f43138e8674ef30deb76f71a6db1bfdd87078e1
Chinedu Mbah <lrrchinedu@gmail.com>
2022-11-04 16:34:04 +0100

    added post endpoint

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const  Util = require('./compiled-js/workings');
+const bodyParser = require('body-parser');
+
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 app.use(cors({
     origin: '*'
@@ -12,14 +17,22 @@ var port = process.env.PORT || 1337;
 
 app.get('/', (req, res) =>{
 
-    res.send({
+    var response = {
         slackUsername: "Ronin",
         backend: true,
         age: 28,
         bio: "Changing the world one semicolon at a time :) "
-    });
-    
+    }
+    res.send(response);
 });
+
+app.post('/op', (req, res) => {
+
+    var result = Util.performOperation(req.body);
+
+    res.send(result)
+})
+
 app.listen(port, () => {
     console.log("Server running");
 });
